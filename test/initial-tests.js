8feff949017e61e1047210134428373305538521
brianc <brian.m.carlson@gmail.com>
2011-12-31 01:07:18 -0600

    initial commit

---
+++ test/initial-tests.js
@@ -0,0 +1,84 @@
+var ami = require(__dirname + '/../lib');
+
+var EventEmitter = require('events').EventEmitter;
+var util = require('util');
+var MockSocket = function() {
+  EventEmitter.call(this);
+  this.data = [];
+}
+
+util.inherits(MockSocket, EventEmitter);
+
+MockSocket.prototype.connect = function(port, host, connectListener) {
+  this.port = port;
+  if('function' === typeof host) {
+    connectListener = host;
+  }
+  else if('function' === typeof connectListener) {
+    this.on('connect', connectListener);
+  }
+  this.host = host;
+}
+
+MockSocket.prototype.write = function(data) {
+  this.data.push(data);
+}
+
+describe('ami.Client', function() {
+  describe('connection', function() {
+    describe('success', function() {
+      var socket = new MockSocket();
+      var client = new ami.Client(socket);
+      var port = 5038;
+      var host = 'localhost';
+
+      before(function(done) {
+        client.connect(port, host, done);
+        process.nextTick(function() {
+          socket.emit('connect');
+        })
+      })
+
+      it('sets socket properties', function() {
+        client.socket.port.should.equal(5038);
+        client.socket.host.should.equal('localhost');
+      })
+
+    })
+
+    describe('error', function() {
+      var socket = new MockSocket();
+      var client = new ami.Client(socket);
+      var port = 5038;
+      var host = 'localhost';
+      
+      it('emits error', function(done) {
+        client.connect(1234);
+        client.on('error', function() {
+          done();
+        });
+        process.nextTick(function() {
+          socket.emit('error', new Error("fake error"));
+        })
+      })
+      
+    })
+
+  })
+
+  describe('login', function() {
+    describe('sent packet', function() {
+      var socket = new MockSocket();
+      var client = new ami.Client(socket);
+      client.login('user', 'pass', function() {
+      });
+      it('has correct information', function() {
+        socket.data[0].should.equal([
+                                    'ActionID: 1',
+                                    'Action: login',
+                                    'Username: user',
+                                    'Secret: pass','',''].join('\r\n'))
+      })
+    })
+  })
+})
